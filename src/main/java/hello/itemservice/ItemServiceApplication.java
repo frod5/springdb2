bfb1e07e8c23804ab68d12ee03d8ae16b815ff3d
sunwooKim <frod5@kakao.com>
2023-04-16 21:59:05 +0900

    스프링 DB접근기술2 - 프로젝트 구조 이해

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -18,7 +18,12 @@ public class ItemServiceApplication {
 	}
 
 	@Bean
-	@Profile("local")
+	@Profile("local") //@Profile("local") : 특정 프로필의 경우에만 해당 스프링 빈을 등록한다
+	//스프링은 로딩 시점에 application.properties 의 spring.profiles.active 속성을 읽어서
+	//프로필로 사용한다.
+	//이 프로필은 로컬(나의 PC), 운영 환경, 테스트 실행 등등 다양한 환경에 따라서 다른 설정을 할 때 사용하는
+	//정보이다.
+	//프로필을 지정하지 않으면 디폴트( default ) 프로필이 실행
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
 	}
--- src/main/java/hello/itemservice/TestDataInit.java
+++ src/main/java/hello/itemservice/TestDataInit.java
@@ -16,7 +16,7 @@ public class TestDataInit {
     /**
      * 확인용 초기 데이터 추가
      */
-    @EventListener(ApplicationReadyEvent.class)
+    @EventListener(ApplicationReadyEvent.class) //스프링 컨테이너가 완전히 초기화를 끝내고, 실행준비가 되었을때 발생하는 이벤트. @PostConstruct을 사용하면 AOP가 모두 적용되지 않을 떄 호출이 될 수도 있다.
     public void initData() {
         log.info("test data init");
         itemRepository.save(new Item("itemA", 10000, 10));
