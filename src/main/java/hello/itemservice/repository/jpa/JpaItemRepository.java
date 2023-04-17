006047c8cdf0d846cfb23682daf6f52c1315b389
sunwooKim <frod5@kakao.com>
2023-04-17 16:01:17 +0900

    스프링 DB접근기술2 - JPA 예외 변환

--- src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -17,6 +17,18 @@ import java.util.Optional;
 @Slf4j
 @Repository
 @Transactional
+//EntityManager 는 순수한 JPA 기술이고, 스프링과는 관계가 없다. 따라서 엔티티 매니저는 예외가 발생하면 JPA 관련 예외를 발생시킨다.
+//JPA는 PersistenceException 과 그 하위 예외를 발생시킨다.
+//추가로 JPA는 IllegalStateException , IllegalArgumentException 을 발생시킬 수 있다.
+//그렇다면 JPA 예외를 스프링 예외 추상화( DataAccessException )로 어떻게 변환할 수 있을까?
+//비밀은 바로 @Repository 에 있다.
+
+//@Repository의 기능
+//@Repository 가 붙은 클래스는 컴포넌트 스캔의 대상이 된다.
+//@Repository 가 붙은 클래스는 예외 변환 AOP의 적용 대상이 된다.
+
+//스프링과 JPA를 함께 사용하는 경우 스프링은 JPA 예외 변환기( PersistenceExceptionTranslator )를 등록한다.
+//예외 변환 AOP 프록시는 JPA 관련 예외가 발생하면 JPA 예외 변환기를 통해 발생한 예외를 스프링 데이터 접근 예외로 변환한다.
 public class JpaItemRepository implements ItemRepository {
 
     private final EntityManager em;
